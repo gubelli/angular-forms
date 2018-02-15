54e74fc8cc5ef148f17b41bd2157f1bde983465e
Martin Leimer <martin.leimer@zuehlke.com>
2018-02-15 10:12:22 +0100

    added general kind of form descriptions

--- src/app/complex-form/complex-form.component.ts
+++ src/app/complex-form/complex-form.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-complex-form',
   templateUrl: './complex-form.component.html',
-  styleUrls: ['./complex-form.component.css']
+  styleUrls: ['./complex-form.component.scss']
 })
 export class ComplexFormComponent implements OnInit {
 
