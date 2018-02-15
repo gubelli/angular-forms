f6abbb3636c6f7581049664ced40e2673fa4e00c
Martin Leimer <martin.leimer@zuehlke.com>
2018-02-15 09:38:17 +0100

    introducing Angular router

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,26 @@
 import {async, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {ComplexFormComponent} from './complex-form/complex-form.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {APP_BASE_HREF} from '@angular/common';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ComplexFormComponent,
+        DashboardComponent,
+        PageNotFoundComponent
       ],
+      imports: [
+        AppRoutingModule
+      ],
+      providers: [
+        {provide: APP_BASE_HREF, useValue: '/'}
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -14,15 +28,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular Forms');
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import {NgModule} from '@angular/core';
 
 
 import {AppComponent} from './app.component';
+import {ComplexFormComponent} from './complex-form/complex-form.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AppRoutingModule} from './app-routing.module';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComplexFormComponent,
+    DashboardComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
