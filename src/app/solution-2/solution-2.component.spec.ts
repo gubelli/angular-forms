d3807ca6137e57281674769346a9f8d5a5472157
Martin Leimer <martin.leimer@zuehlke.com>
2018-03-27 18:40:19 +0200

    added tests and improved solution

--- src/app/solution-2/solution-2.component.spec.ts
+++ src/app/solution-2/solution-2.component.spec.ts
@@ -23,5 +23,28 @@ describe('Solution2Component', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.form).toBeTruthy();
+  });
+
+  it('should not save when no input is entered', () => {
+    expect(component.formData).toBeUndefined();
+    component.onSubmit();
+    fixture.detectChanges();
+    expect(component.form.invalid).toBe(true);
+    expect(component.formData).toBeUndefined();
+  });
+
+  it('should save when each input is valid', () => {
+    const expectedFormData = {
+      firstName: 'Max',
+      lastName: 'Mustermann'
+    };
+    expect(component.formData).toBeUndefined();
+
+    component.form.patchValue(expectedFormData);
+    component.onSubmit();
+
+    expect(component.form.valid).toBe(true);
+    expect(component.formData).toEqual(expectedFormData);
   });
 });
